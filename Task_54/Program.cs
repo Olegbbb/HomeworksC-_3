3a99449087e24270a23c72434b086b3bd0fcee84
Oleg <apricot-ob@yandex.ru>
2022-11-14 22:42:59 +0300

    Задача 54 - 8 Семинар

---
+++ Task_54/Program.cs
@@ -0,0 +1,52 @@
+﻿void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 10);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ReleaseMatrix(int[,] matrix)
+{
+    int temp = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int c = 0; c < matrix.GetLength(1) - 1; c++)
+            {
+                if (matrix[i, c] < matrix[i, c + 1])
+                {
+                    temp = matrix[i, c];
+                    matrix[i, c] = matrix[i, c + 1];
+                    matrix[i, c + 1] = temp;
+                }
+            }
+        }
+    }
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите кол-во строк и столбцов: ");
+string[] num = Console.ReadLine()!.Split(' ');
+int n = int.Parse(num[0]);
+int m = int.Parse(num[1]);
+int[,] matrix = new int[n, m];
+Console.WriteLine("Начальный массив: ");
+InputMatrix(matrix);
+Console.WriteLine("Конечный массив: ");
+ReleaseMatrix(matrix);
+
