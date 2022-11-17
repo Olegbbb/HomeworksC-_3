59e59dfe37c444e200b0377784388de0c937eb03
Oleg <apricot-ob@yandex.ru>
2022-11-17 21:34:38 +0300

    задача 66 - 9 Семинар

---
+++ Task_66/Program.cs
@@ -0,0 +1,17 @@
+﻿int FindSum(int n, int m)
+{
+    if (n == m)
+        return n;
+    return FindSum(n, m - 1) + m;
+}
+Console.Clear();
+Console.Write("Введите двачисла: ");
+string[] num = Console.ReadLine()!.Split(' ');
+int n = int.Parse(num[0]);
+int m = int.Parse(num[1]);
+if (n > m)
+{
+    int temp = 0;
+    temp = n; n = m; m = temp;
+}
+Console.WriteLine($"Сумма равна: {FindSum(n, m)}");
